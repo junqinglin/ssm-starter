4906ef95f00280a0b177e38f9cd8698b794c7ee5
Qing0299 <510681621@qq.com>
2023-09-21 19:29:24 +0800

    commit src file

---
+++ src/main/java/com/ljq/config/ServletConfig.java
@@ -0,0 +1,28 @@
+package com.ljq.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    //设置post请求中文乱码过滤器
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("utf-8");
+        return new Filter[]{filter};
+    }
+}
---
+++ src/main/java/com/ljq/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package com.ljq.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan({"com.ljq.controller", "com.ljq.config"})
+@EnableWebMvc
+public class SpringMvcConfig {
+}
